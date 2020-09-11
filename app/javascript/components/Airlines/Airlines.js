01e6d2a51adbd38bc6225853166ad668b752f1ac
Geraldine Galue <geraldinegalue@gmail.com>
2020-09-11 18:10:14 -0400

    Add react-router-dom and create Airline components

---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+
+const Airlines = () => {
+	const [airlines, setAirlines] = useState([])
+
+	useEffect(() => {
+		// Get all airlines from API
+		// Update airlines in our state
+
+		axios.get('/api/v1/airlines.json')
+		.then(resp => {
+			setAirlines(resp.data.data)
+		})
+		.catch(resp => console.log(resp) )
+	}, [airlines.length])
+
+	const list = airlines.map( item => {
+		return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+	})
+
+	return (
+		<div className="home">
+			<div className="header">
+				<h1>OpenFlights</h1>
+				<div className="subheader">Honest, unbiased airline reviews.</div>
+			</div>
+			<div className="grid">
+				<ul>{list}</ul>
+			</div>
+		</div>
+	)
+}
+
+export default Airlines
